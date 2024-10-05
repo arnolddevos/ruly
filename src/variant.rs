34aae8d0fa1600df81f600980eecb76b07482805
Arnold deVos <adv+gh@backgroundsignal.com>
2024-10-05 14:32:23 +1000

    additional conversions and serde for variant

--- src/variant.rs
+++ src/variant.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, NaiveDate, Utc};
 use derive_more::derive::{Display, From, TryInto};
+use serde::Deserialize;
 use serde::Serialize;
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -12,7 +13,7 @@ use std::fmt::Display;
 /// - `Table` variants are by joining their values by key.
 /// - Scalar variants are joined if equal.
 /// - Other pairs result in a `Conflict` which is the top of the join lattice.   
-#[derive(Serialize, Clone, Debug, From, TryInto, Display)]
+#[derive(Serialize, Deserialize, Clone, Debug, From, TryInto, Display)]
 pub enum Variant {
     /// Top of the join lattice
     #[display("conflict {} {}", _0, _1)]
@@ -100,7 +101,7 @@ impl Lattice for Variant {
 }
 
 /// A set of `Ident`s.  This implements `Lattice` and `join` is by set union.
-#[derive(Serialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Set(HashSet<Ident>);
 
 impl Lattice for Set {
@@ -129,7 +130,7 @@ impl Display for Set {
 /// and the corresponding `Variant` represents an `A` value.
 ///
 /// `Table` implements `Lattice`.  Joining a table joins values of the same key.
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Table(HashMap<Ident, Variant>);
 
 impl Table {
@@ -170,17 +171,66 @@ impl Lattice for Table {
 }
 
 /// An `Ident` identifies a property or an element of a set.
-#[derive(PartialEq, Eq, Hash, Debug, Display, From, Clone, Serialize)]
+#[derive(PartialEq, Eq, Hash, Debug, Display, From, Clone)]
 pub enum Ident {
     NonIntern(String),
     Intern(&'static str),
     Anonymous(u64),
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ExternalIdent {
+    NonIntern(String),
+    Anonymous(u64),
+}
+
+impl Serialize for Ident {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let x = match self {
+            Ident::NonIntern(i) => ExternalIdent::NonIntern(i.clone()),
+            Ident::Intern(i) => ExternalIdent::NonIntern(i.to_string()),
+            Ident::Anonymous(i) => ExternalIdent::Anonymous(*i),
+        };
+        x.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Ident {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let x: ExternalIdent = ExternalIdent::deserialize(deserializer)?;
+        match x {
+            ExternalIdent::NonIntern(i) => Ok(Ident::NonIntern(i)),
+            ExternalIdent::Anonymous(i) => Ok(Ident::Anonymous(i)),
+        }
+    }
+}
+
 /// A skeleton Error type
-#[derive(Debug, Clone, Display, From, Serialize)]
+#[derive(Debug, Clone, Display, From, Serialize, Deserialize)]
 pub enum Error {
     Detail(String),
 }
 
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Error::Detail(value.to_string())
+    }
+}
+
 impl std::error::Error for Error {}
+
+impl<A: Into<Variant>> From<Result<A, Error>> for Variant {
+    fn from(value: Result<A, Error>) -> Self {
+        match value {
+            Ok(a) => a.into(),
+            Err(e) => e.into(),
+        }
+    }
+}
