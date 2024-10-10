33808abf74c0280d7768245d37d192d5f471552d
Arnold deVos <adv+gh@backgroundsignal.com>
2024-10-11 08:42:58 +1100

    reintroduce the Quantity abstraction

--- src/quantity/money.rs
+++ src/quantity/money.rs
@@ -1,3 +1,5 @@
+use crate::quantity::Quantity;
+use crate::variant::Error;
 use nom::{
     bytes::complete::tag,
     character::complete::{digit1, multispace0, one_of},
@@ -5,151 +7,102 @@ use nom::{
     sequence::{preceded, terminated, tuple},
 };
 
-use crate::variant::{Error, Variant};
-use std::{fmt::Display, marker::PhantomData, str::FromStr};
-
-/// Marks a type (usually a unit type) as representing a currency such as
-/// US dollars or Euros.  Members of this trait give the fixed characteristics
-/// of a currency.  So far, that is just the symbol.
-pub trait Currency {
-    const SYMBOL: &'static str;
-    const PRECISION: u32;
-    const FACTOR: i64 = 10i64.pow(Self::PRECISION);
-}
-
 /// Australian currency.
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct AUD;
 
-impl Currency for AUD {
-    const SYMBOL: &'static str = "$";
-    const PRECISION: u32 = 2;
+impl Quantity for AUD {
+    type Repr = i64;
+
+    fn parse(text: &str) -> Result<Self::Repr, Error> {
+        money_from_str::<2>(text, "$")
+    }
+
+    fn format(value: &Self::Repr) -> String {
+        money_to_str::<2>(*value, "$")
+    }
 }
 
 /// US currency.
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct USD;
 
-impl Currency for USD {
-    const SYMBOL: &'static str = "$";
-    const PRECISION: u32 = 2;
+impl Quantity for USD {
+    type Repr = i64;
+
+    fn parse(text: &str) -> Result<Self::Repr, Error> {
+        money_from_str::<2>(text, "$")
+    }
+
+    fn format(value: &Self::Repr) -> String {
+        money_to_str::<2>(*value, "$")
+    }
 }
 
-/// Represents an amount of money in a given currency.
-/// The currency is a type parameter and money of different
-/// currencies are not comparable.
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Money<C> {
-    cents: i64,
-    currency: PhantomData<C>,
-}
+fn money_from_str<const PRECISION: u32>(input: &str, symbol: &str) -> Result<i64, Error> {
+    let error = "error in money value";
+    let spaces = multispace0::<&str, nom::error::Error<&str>>;
+    let digits = digit1::<&str, nom::error::Error<&str>>;
 
-impl<C> Money<C> {
-    pub fn from_repr(cents: i64) -> Self {
-        Self {
-            cents,
-            currency: PhantomData,
+    let (_, (sign1, _, sign2, whole, frac)) = all_consuming(terminated(
+        tuple((
+            opt(terminated(one_of("+-"), spaces)),
+            opt(terminated(tag(symbol), spaces)),
+            opt(terminated(one_of("+-"), spaces)),
+            opt(digits),
+            opt(preceded(tag("."), opt(digits))),
+        )),
+        spaces,
+    ))(input)
+    .or(Err(error))?;
+
+    let mut value: i64 = 0;
+
+    if let Some(x) = whole {
+        value += x.parse::<i64>().or(Err(error))? * 10i64.pow(PRECISION);
+    }
+
+    if let Some(Some(x)) = frac {
+        let mut frac = x.parse::<i64>().or(Err(error))?;
+        let prec = x.len() as u32;
+        if prec < PRECISION {
+            frac *= 10i64.pow(PRECISION - prec);
+        } else if prec > PRECISION {
+            frac /= 10i64.pow(prec - PRECISION);
         }
+        value += frac;
     }
-}
 
-impl<C> FromStr for Money<C>
-where
-    C: Currency,
-{
-    type Err = Error;
-
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let error = "error in money value";
-        let spaces = multispace0::<&str, nom::error::Error<&str>>;
-        let digits = digit1::<&str, nom::error::Error<&str>>;
-
-        let (_, (sign1, _, sign2, whole, frac)) = all_consuming(terminated(
-            tuple((
-                opt(terminated(one_of("+-"), spaces)),
-                opt(terminated(tag(C::SYMBOL), spaces)),
-                opt(terminated(one_of("+-"), spaces)),
-                opt(digits),
-                opt(preceded(tag("."), opt(digits))),
-            )),
-            spaces,
-        ))(input)
-        .or(Err(error))?;
-
-        let mut value: i64 = 0;
-
-        if let Some(x) = whole {
-            value += x.parse::<i64>().or(Err(error))? * C::FACTOR;
+    match (sign1, sign2) {
+        (Some('-'), None) | (None, Some('-')) => {
+            value = -value;
         }
-
-        if let Some(Some(x)) = frac {
-            let mut frac = x.parse::<i64>().or(Err(error))?;
-            let prec = x.len() as u32;
-            if prec < C::PRECISION {
-                frac *= 10i64.pow(C::PRECISION - prec);
-            } else if prec > C::PRECISION {
-                frac /= 10i64.pow(prec - C::PRECISION);
-            }
-            value += frac;
-        }
-
-        match (sign1, sign2) {
-            (Some('-'), None) | (None, Some('-')) => {
-                value = -value;
-            }
-            (None, None) => {}
-            _ => Err(error)?,
-        }
-
-        Ok(Self {
-            cents: value,
-            currency: PhantomData,
-        })
+        (None, None) => {}
+        _ => Err(error)?,
     }
+
+    Ok(value)
 }
 
-impl<C> Display for Money<C>
-where
-    C: Currency,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let sign = if self.cents < 0 { "-" } else { "" };
-        let magnitude = format!("{:01$}", self.cents.abs(), (C::PRECISION + 1) as usize);
-        let split = magnitude.len() - C::PRECISION as usize;
-        f.write_fmt(format_args!(
-            "{}{}{}.{}",
-            sign,
-            C::SYMBOL,
-            &magnitude[..split],
-            &magnitude[split..]
-        ))
-    }
-}
-
-impl<C> From<Money<C>> for Variant {
-    fn from(value: Money<C>) -> Self {
-        value.cents.into()
-    }
-}
-
-impl<C> TryFrom<Variant> for Money<C> {
-    type Error = Error;
-
-    fn try_from(value: Variant) -> Result<Self, Self::Error> {
-        Ok(Self {
-            cents: value.try_into().or(Err("incorrect variant for money"))?,
-            currency: PhantomData,
-        })
-    }
+fn money_to_str<const PRECISION: u32>(value: i64, symbol: &str) -> String {
+    let sign = if value < 0 { "-" } else { "" };
+    let magnitude = format!("{:01$}", value.abs(), (PRECISION + 1) as usize);
+    let split = magnitude.len() - PRECISION as usize;
+    format!(
+        "{}{}{}.{}",
+        sign,
+        symbol,
+        &magnitude[..split],
+        &magnitude[split..]
+    )
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::quantity::Value;
 
     #[test]
-    fn currency_formating() {
-        type C = Money<AUD>;
+    fn currency_value_formating() {
+        type C = Value<AUD>;
         assert_eq!(C::from_repr(9).to_string(), "$0.09");
         assert_eq!(C::from_repr(-9).to_string(), "-$0.09");
         assert_eq!(C::from_repr(79).to_string(), "$0.79");
@@ -161,8 +114,8 @@ mod test {
     }
 
     #[test]
-    fn currency_parsing() {
-        type C = Money<AUD>;
+    fn currency_value_parsing() {
+        type C = Value<AUD>;
         assert_eq!(C::from_repr(9), "$0.09".parse::<C>().unwrap());
         assert_eq!(C::from_repr(-9), "-$0.09".parse::<C>().unwrap());
         assert_eq!(C::from_repr(79), "$0.79".parse::<C>().unwrap());
